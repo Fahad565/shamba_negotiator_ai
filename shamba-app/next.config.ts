b7dbc2a1080aad25f09aa2d6eca45b52b60ec2d9
Fahad <musafahad2@gmail.com>
2026-04-25 12:59:29 +0300

    fix: move allowedDevOrigins to top-level and added ts-ignore to fix config types

--- shamba-app/next.config.ts
+++ shamba-app/next.config.ts
@@ -3,10 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'standalone',
   // Allow mobile testing on the local network
+  // @ts-ignore
+  allowedDevOrigins: ['192.168.43.228', 'localhost:3000'],
   serverExternalPackages: [],
-  experimental: {
-    allowedDevOrigins: ['192.168.43.228', 'localhost:3000'],
-  }
 };
 
 export default nextConfig;
